1333fb0328ede903f4cd4d00eded5e47da4f5455
wangsitan <wangsitan@aliyun.com>
2025-09-02 11:40:25 +0800

    let main window have frame

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import japInput
 import sys
 
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = japInput.JapInput()
     mainWindow.setAttribute(Qt.WA_TranslucentBackground, True)
-    mainWindow.setWindowFlags(Qt.FramelessWindowHint)
+
+    #mainWindow.setWindowFlags(Qt.FramelessWindowHint)  # no frame
+    #mainWindow.setWindowFlags(QtCore.Qt.Window)  # let main window have frame
+
     mainWindow.setWindowTitle('Jap Input')
     mainWindow.setWindowIcon(QIcon('assert/shuoGG_re.ico'))
     mainWindow.show()
